e4acaecabf8765d770591bac021bf89881d414ba
roknuzzaman <rokon.raz@gmail.com>
2025-02-12 14:19:49 +0600

    Add get a row information function,

--- main.py
+++ main.py
@@ -21,3 +21,15 @@ session = Session()
 
 vervotech_mapping = Table("vervotech_mapping_2", metadata, autoload_with=engine)
 global_hotel_mapping = Table("global_hotel_mapping", metadata, autoload_with=engine)
+
+
+
+def get_a_column_info_follow_a_id(unica_id):
+    query = (
+        vervotech_mapping
+        .select()
+        .with_only_columns(vervotech_mapping.c.ProviderHotelId, vervotech_mapping.c.ProviderFamily) 
+        .where(vervotech_mapping.c.UnicaId == unica_id)
+    )
+    result = session.execute(query).mappings().all()
+    return result
