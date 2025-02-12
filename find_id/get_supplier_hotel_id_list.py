7306bd14503936ec9747f066a08eeb6469f3168e
roknuzzaman <rokon.raz@gmail.com>
2025-02-12 10:56:15 +0600

    create get hotel id list using supplier.

---
+++ find_id/get_supplier_hotel_id_list.py
@@ -0,0 +1,44 @@
+from sqlalchemy import MetaData, Table, create_engine, select, update
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db_host = os.getenv('DB_HOST')
+db_user = os.getenv('DB_USER')
+db_pass = os.getenv('DB_PASSWORD')
+db_name = os.getenv('DB_NAME')
+
+connection_string = f"mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}"
+engine = create_engine(connection_string)
+metadata = MetaData()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+global_hotel_mapping = Table("global_hotel_mapping", metadata, autoload_with=engine)
+
+def get_unique_id_list(supplier):
+    query = (
+        select(global_hotel_mapping.c[supplier])
+        .distinct()
+        .where(global_hotel_mapping.c[supplier].isnot(None))
+        .where(global_hotel_mapping.c.mapStatus != 'G-Done')
+    )
+
+    result = session.execute(query).scalars().all()
+    return result
+    
+def save_id_list_to_file(supplier):
+    unique_ids = get_unique_id_list(supplier)
+    file_name = f"D:/Rokon/ofc_git/global_hotel_mapping/static/file/{supplier}_supplier__hotel_id_list.txt"
+    
+    with open(file_name, "w") as file:
+        for unica_id in unique_ids:
+            file.write(f"{unica_id}\n")
+    print(len(unique_ids))
+    print(f"Unique IDs saved to {file_name}")
+
+supplier = "goglobal"
+save_id_list_to_file(supplier)
+
